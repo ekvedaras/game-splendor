a6dd6acf1ea4b56787cd49018eea56e39689aafe
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2026-07-15 23:30:20 +0300

    the rest of the card deck

--- src/Deck/ClosedDeck.php
+++ src/Deck/ClosedDeck.php
@@ -78,15 +78,51 @@ final class ClosedDeck
         return new self(
             level: Level::First,
             cards: [
-                   new Card(Level::First, Resource::Black, Resource::oneOfAll(), 0),
-                   new Card(Level::First, Resource::Black, Resource::cost(0, 0, 1, 0, 1), 0),
-                   new Card(Level::First, Resource::Black, Resource::cost(0, 2, 0, 0, 2), 0),
-                   new Card(Level::First, Resource::Black, Resource::cost(1, 0, 3, 0, 1), 0),
-                   new Card(Level::First, Resource::Black, Resource::cost(0, 0, 0, 0, 3), 0),
-                   new Card(Level::First, Resource::Black, Resource::cost(0, 1, 1, 2, 1), 0),
-                   new Card(Level::First, Resource::Black, Resource::cost(0, 2, 1, 2, 0), 0),
-                   new Card(Level::First, Resource::Black, Resource::cost(0, 0, 0, 0, 4), 1),
-                //...
+                       // Black reward
+                       new Card(Level::First, Resource::Black, Resource::cost(0, 0, 1, 1, 1), 0),
+                       new Card(Level::First, Resource::Black, Resource::cost(0, 0, 1, 1, 2), 0),
+                       new Card(Level::First, Resource::Black, Resource::cost(0, 0, 2, 1, 2), 0),
+                       new Card(Level::First, Resource::Black, Resource::cost(0, 0, 1, 3, 1), 0),
+                       new Card(Level::First, Resource::Black, Resource::cost(0, 0, 0, 2, 1), 0),
+                       new Card(Level::First, Resource::Black, Resource::cost(0, 0, 0, 2, 2), 0),
+                       new Card(Level::First, Resource::Black, Resource::cost(0, 0, 0, 3, 0), 0),
+                       new Card(Level::First, Resource::Black, Resource::cost(0, 0, 0, 4, 0), 1),
+                       // Blue reward
+                       new Card(Level::First, Resource::Blue, Resource::cost(0, 1, 1, 0, 1), 0),
+                       new Card(Level::First, Resource::Blue, Resource::cost(0, 1, 2, 0, 1), 0),
+                       new Card(Level::First, Resource::Blue, Resource::cost(0, 1, 2, 0, 2), 0),
+                       new Card(Level::First, Resource::Blue, Resource::cost(0, 1, 3, 0, 1), 0),
+                       new Card(Level::First, Resource::Blue, Resource::cost(0, 1, 0, 0, 2), 0),
+                       new Card(Level::First, Resource::Blue, Resource::cost(0, 2, 0, 0, 2), 0),
+                       new Card(Level::First, Resource::Blue, Resource::cost(0, 3, 0, 0, 0), 0),
+                       new Card(Level::First, Resource::Blue, Resource::cost(0, 4, 0, 0, 0), 1),
+                       // White reward
+                       new Card(Level::First, Resource::White, Resource::cost(0, 0, 1, 1, 1), 0),
+                       new Card(Level::First, Resource::White, Resource::cost(0, 0, 2, 1, 1), 0),
+                       new Card(Level::First, Resource::White, Resource::cost(0, 0, 2, 2, 1), 0),
+                       new Card(Level::First, Resource::White, Resource::cost(0, 0, 1, 1, 3), 0),
+                       new Card(Level::First, Resource::White, Resource::cost(0, 0, 0, 2, 1), 0),
+                       new Card(Level::First, Resource::White, Resource::cost(0, 0, 0, 2, 2), 0),
+                       new Card(Level::First, Resource::White, Resource::cost(0, 0, 0, 3, 0), 0),
+                       new Card(Level::First, Resource::White, Resource::cost(0, 0, 0, 4, 0), 1),
+                       // Green reward
+                       new Card(Level::First, Resource::Green, Resource::cost(0, 1, 1, 0, 1), 0),
+                       new Card(Level::First, Resource::Green, Resource::cost(0, 1, 1, 0, 2), 0),
+                       new Card(Level::First, Resource::Green, Resource::cost(0, 2, 1, 0, 2), 0),
+                       new Card(Level::First, Resource::Green, Resource::cost(0, 1, 3, 0, 1), 0),
+                       new Card(Level::First, Resource::Green, Resource::cost(0, 2, 1, 0, 1), 0),
+                       new Card(Level::First, Resource::Green, Resource::cost(0, 2, 2, 0, 0), 0),
+                       new Card(Level::First, Resource::Green, Resource::cost(0, 3, 0, 0, 0), 0),
+                       new Card(Level::First, Resource::Green, Resource::cost(0, 4, 0, 0, 0), 1),
+                       // Red reward
+                       new Card(Level::First, Resource::Red, Resource::cost(0, 1, 0, 1, 1), 0),
+                       new Card(Level::First, Resource::Red, Resource::cost(0, 2, 0, 1, 1), 0),
+                       new Card(Level::First, Resource::Red, Resource::cost(0, 2, 0, 2, 1), 0),
+                       new Card(Level::First, Resource::Red, Resource::cost(0, 1, 0, 3, 1), 0),
+                       new Card(Level::First, Resource::Red, Resource::cost(0, 0, 0, 2, 1), 0),
+                       new Card(Level::First, Resource::Red, Resource::cost(0, 0, 0, 2, 2), 0),
+                       new Card(Level::First, Resource::Red, Resource::cost(0, 0, 0, 3, 0), 0),
+                       new Card(Level::First, Resource::Red, Resource::cost(0, 0, 0, 4, 0), 1),
                    ],
         );
     }
@@ -96,14 +132,41 @@ final class ClosedDeck
         return new self(
             level: Level::Second,
             cards: [
-                       new Card(Level::Second, Resource::Black, Resource::oneOfAll(), 0),
-                       new Card(Level::Second, Resource::Black, Resource::cost(0, 0, 1, 0, 1), 0),
-                       new Card(Level::Second, Resource::Black, Resource::cost(0, 2, 0, 0, 2), 0),
-                       new Card(Level::Second, Resource::Black, Resource::cost(1, 0, 3, 0, 1), 0),
-                       new Card(Level::Second, Resource::Black, Resource::cost(0, 0, 0, 0, 3), 0),
-                       new Card(Level::Second, Resource::Black, Resource::cost(0, 1, 1, 2, 1), 0),
-                       new Card(Level::Second, Resource::Black, Resource::cost(0, 2, 1, 2, 0), 0),
-                       new Card(Level::Second, Resource::Black, Resource::cost(0, 0, 0, 0, 4), 1),
+                       // Black reward
+                       new Card(Level::Second, Resource::Black, Resource::cost(0, 3, 0, 2, 2), 1),
+                       new Card(Level::Second, Resource::Black, Resource::cost(0, 3, 0, 3, 2), 1),
+                       new Card(Level::Second, Resource::Black, Resource::cost(0, 2, 0, 4, 1), 2),
+                       new Card(Level::Second, Resource::Black, Resource::cost(0, 3, 0, 5, 0), 2),
+                       new Card(Level::Second, Resource::Black, Resource::cost(0, 5, 0, 0, 0), 2),
+                       new Card(Level::Second, Resource::Black, Resource::cost(0, 0, 0, 6, 0), 3),
+                       // Blue reward
+                       new Card(Level::Second, Resource::Blue, Resource::cost(0, 0, 3, 2, 2), 1),
+                       new Card(Level::Second, Resource::Blue, Resource::cost(0, 0, 3, 3, 2), 1),
+                       new Card(Level::Second, Resource::Blue, Resource::cost(0, 0, 0, 5, 3), 2),
+                       new Card(Level::Second, Resource::Blue, Resource::cost(0, 2, 1, 4, 0), 2),
+                       new Card(Level::Second, Resource::Blue, Resource::cost(0, 0, 0, 5, 0), 2),
+                       new Card(Level::Second, Resource::Blue, Resource::cost(0, 0, 0, 6, 0), 3),
+                       // White reward
+                       new Card(Level::Second, Resource::White, Resource::cost(0, 0, 3, 2, 2), 1),
+                       new Card(Level::Second, Resource::White, Resource::cost(0, 0, 3, 3, 2), 1),
+                       new Card(Level::Second, Resource::White, Resource::cost(0, 0, 1, 4, 2), 2),
+                       new Card(Level::Second, Resource::White, Resource::cost(0, 0, 0, 5, 3), 2),
+                       new Card(Level::Second, Resource::White, Resource::cost(0, 0, 0, 5, 0), 2),
+                       new Card(Level::Second, Resource::White, Resource::cost(0, 0, 0, 0, 6), 3),
+                       // Green reward
+                       new Card(Level::Second, Resource::Green, Resource::cost(0, 3, 2, 2, 0), 1),
+                       new Card(Level::Second, Resource::Green, Resource::cost(0, 2, 3, 3, 0), 1),
+                       new Card(Level::Second, Resource::Green, Resource::cost(0, 4, 2, 0, 1), 2),
+                       new Card(Level::Second, Resource::Green, Resource::cost(0, 0, 3, 5, 0), 2),
+                       new Card(Level::Second, Resource::Green, Resource::cost(0, 0, 0, 5, 0), 2),
+                       new Card(Level::Second, Resource::Green, Resource::cost(0, 0, 0, 6, 0), 3),
+                       // Red reward
+                       new Card(Level::Second, Resource::Red, Resource::cost(0, 2, 2, 0, 3), 1),
+                       new Card(Level::Second, Resource::Red, Resource::cost(0, 2, 3, 0, 3), 1),
+                       new Card(Level::Second, Resource::Red, Resource::cost(0, 1, 4, 0, 2), 2),
+                       new Card(Level::Second, Resource::Red, Resource::cost(0, 3, 5, 0, 0), 2),
+                       new Card(Level::Second, Resource::Red, Resource::cost(0, 5, 0, 0, 0), 2),
+                       new Card(Level::Second, Resource::Red, Resource::cost(0, 6, 0, 0, 0), 3),
                    ],
         );
     }
@@ -113,10 +176,31 @@ final class ClosedDeck
         return new self(
             level: Level::Third,
             cards: [
+                       // Black reward
                        new Card(Level::Third, Resource::Black, Resource::cost(0, 3, 3, 3, 5), 3),
                        new Card(Level::Third, Resource::Black, Resource::cost(0, 0, 0, 7, 0), 4),
-                       new Card(Level::Third, Resource::Black, Resource::cost(3, 0, 6, 0, 3), 4),
-                       new Card(Level::Third, Resource::Black, Resource::cost(3, 0, 7, 0, 0), 5),
+                       new Card(Level::Third, Resource::Black, Resource::cost(3, 0, 3, 6, 0), 4),
+                       new Card(Level::Third, Resource::Black, Resource::cost(3, 0, 0, 7, 0), 5),
+                       // Blue reward
+                       new Card(Level::Third, Resource::Blue, Resource::cost(0, 3, 3, 0, 5), 3),
+                       new Card(Level::Third, Resource::Blue, Resource::cost(0, 0, 0, 0, 7), 4),
+                       new Card(Level::Third, Resource::Blue, Resource::cost(3, 0, 0, 0, 6), 4),
+                       new Card(Level::Third, Resource::Blue, Resource::cost(3, 0, 0, 0, 7), 5),
+                       // White reward
+                       new Card(Level::Third, Resource::White, Resource::cost(0, 0, 5, 3, 3), 3),
+                       new Card(Level::Third, Resource::White, Resource::cost(0, 0, 0, 0, 7), 4),
+                       new Card(Level::Third, Resource::White, Resource::cost(3, 0, 0, 3, 6), 4),
+                       new Card(Level::Third, Resource::White, Resource::cost(3, 0, 0, 0, 7), 5),
+                       // Green reward
+                       new Card(Level::Third, Resource::Green, Resource::cost(0, 5, 3, 3, 0), 3),
+                       new Card(Level::Third, Resource::Green, Resource::cost(0, 0, 0, 0, 7), 4),
+                       new Card(Level::Third, Resource::Green, Resource::cost(3, 3, 0, 0, 6), 4),
+                       new Card(Level::Third, Resource::Green, Resource::cost(3, 0, 0, 0, 7), 5),
+                       // Red reward
+                       new Card(Level::Third, Resource::Red, Resource::cost(0, 3, 3, 5, 3), 3),
+                       new Card(Level::Third, Resource::Red, Resource::cost(0, 0, 0, 0, 7), 4),
+                       new Card(Level::Third, Resource::Red, Resource::cost(3, 0, 0, 6, 3), 4),
+                       new Card(Level::Third, Resource::Red, Resource::cost(0, 0, 0, 7, 3), 5),
                    ],
         );
     }
